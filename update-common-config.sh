cbf1133531e01828c7ada677affdef53e7951747
Michał Łyszczek <michal.lyszczek@bofc.pl>
2017-12-23 10:13:38 +0100

    Changed update common config to update config

--- update-common-config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-grep -A999999 "##### COMMON SECTION START #####" .config > configs/common.in
---
+++ update-config.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+common()
+{
+    sed -n "/# $1/,/# $2/p" .config >> configs/common.in
+}
+
+arch()
+{
+    sed -n "/# $1/,/# $2/p" .config >> configs/config.${a}.in
+}
+
+a=`grep "BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE" .config | cut -f2 -d.`
+echo > configs/common.in
+echo > configs/config.${a}.in
+
+common "Automatically generated file; DO NOT EDIT" "Target options"
+common "Build options" "Toolchain"
+common "System configuration" "Kernel"
+common "Target packages" "Legacy options removed in"
+
+arch "Target options" "Build options"
+arch "Toolchain" "System configuration"
+arch "Kernel" "Target packages"
