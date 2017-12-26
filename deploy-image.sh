52065cbc96b94a2f4f4dd17d0cec1e0be0b20d28
Michał Łyszczek <michal.lyszczek@bofc.pl>
2017-12-26 15:39:07 +0100

    Added armv7a9 architecture

--- deploy-image.sh
+++ deploy-image.sh
@@ -5,12 +5,19 @@ kernel_path=/boot/qemu
 kernel_version=`grep BR2_LINUX_KERNEL_VERSION .config | cut -f2 -d\"`
 rootfs_path=/var/diskless/images/bbs-${target}
 rootfs_server=root@kurwik
-
+zimage=`ls -1 output/images | grep zImage`
+dtb=`ls -1 output/images | grep .dtb`
 
 echo "This is very-local script check paths before you continue! Will execute:"
 
 echo -e "\tpv output/images/rootfs.tar | ssh ${rootfs_server} \"tar xf - -C ${rootfs_path}\""
-echo -e "\tsudo cp output/images/zImage ${kernel_path}/bbs-${target}-${kernel_version}"
+echo -e "\tsudo cp output/images/${zimage} ${kernel_path}/bbs-${target}-${kernel_version}"
+
+if [ ! -z "${dtb}" ]
+then
+    echo -e "\tsudo cp output/images/${dtb} ${kernel_path}/bbs-${target}-${kernel_version}.dtb"
+fi
+
 
 echo ""
 echo "Hit enter if this looks good to you. Otherwise edit this scripts settings"
@@ -18,5 +25,12 @@ echo "Hit enter if this looks good to you. Otherwise edit this scripts settings"
 read
 
 pv output/images/rootfs.tar | ssh ${rootfs_server} "tar xf - -C ${rootfs_path}"
-sudo cp output/images/bzImage ${kernel_path}/bbs-${target}-${kernel_version}
+sudo cp output/images/${zimage} ${kernel_path}/bbs-${target}-${kernel_version}
 sudo chown qemu:qemu ${kernel_path}/bbs-${target}-${kernel_version}
+
+if [ ! -z "${dtb}" ]
+then
+    sudo cp output/images/${dtb} ${kernel_path}/bbs-${target}-${kernel_version}.dtb
+    sudo chown qemu:qemu ${kernel_path}/bbs-${target}-${kernel_version}.dtb
+fi
+
