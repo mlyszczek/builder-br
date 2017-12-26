e1c1ced67692e4d4fd3c18603e3639b43c904de7
Michał Łyszczek <michal.lyszczek@bofc.pl>
2017-12-26 10:53:12 +0100

    refactoring in target names area

--- board/builder/pre-image.sh
+++ board/builder/pre-image.sh
@@ -2,29 +2,14 @@
 
 target_dir=$1
 
-arch=`grep BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE ${BR2_CONFIG}  | cut -f2 -d.`
+target=`grep BR2_TOOLCHAIN_EXTERNAL_PREFIX=\" ${BR2_CONFIG}  | cut -f2 -d\"`
+arch=`echo ${target} | cut -f1 -d-`
+hostname="bbs-${target}"
 
 
-set_hostname()
-{
-    hostname="bbs-${arch}-linux"
-    echo $hostname
-    echo $hostname > ${target_dir}/etc/hostname
-}
-
-create_hosts()
-{
-    hostname=`head -c -1 -q ${target_dir}/etc/hostname`
-    echo "127.0.0.1  ${hostname}.kurwinet.pl ${hostname} localhost" > ${target_dir}/etc/hosts
-}
-
-update_opkg_with_arch()
-{
-    echo "arch ${arch} 200" >> ${target_dir}/etc/opkg/opkg.conf
-}
-
-set_hostname
-create_hosts
-update_opkg_with_arch
+echo $hostname > ${target_dir}/etc/hostname
+echo "127.0.0.1  ${hostname}.kurwinet.pl ${hostname} localhost" > ${target_dir}/etc/hosts
+echo "arch ${arch} 200" >> ${target_dir}/etc/opkg/opkg.conf
+sed -i "s/\${TARGET}/${target}/g" ${target_dir}/var/lib/buildbot/buildbot.tac
 
 exit 0
--- deploy-image.sh
+++ deploy-image.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-arch=`grep "BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE" .config | cut -f2 -d.`
+target=`grep BR2_TOOLCHAIN_EXTERNAL_PREFIX=\" .config | cut -f2 -d\"`
 kernel_path=/boot/qemu
 kernel_version=`grep BR2_LINUX_KERNEL_VERSION .config | cut -f2 -d\"`
-rootfs_path=/var/diskless/images/bbs-${arch}-linux
+rootfs_path=/var/diskless/images/bbs-${target}
 rootfs_server=root@kurwik
 
 
 echo "This is very-local script check paths before you continue! Will execute:"
 
 echo -e "\tpv output/images/rootfs.tar | ssh ${rootfs_server} \"tar xf - -C ${rootfs_path}\""
-echo -e "\tsudo cp output/images/zImage ${kernel_path}/bbs-${arch}-linux-${kernel_version}"
+echo -e "\tsudo cp output/images/zImage ${kernel_path}/bbs-${target}-${kernel_version}"
 
 echo ""
 echo "Hit enter if this looks good to you. Otherwise edit this scripts settings"
@@ -18,5 +18,5 @@ echo "Hit enter if this looks good to you. Otherwise edit this scripts settings"
 read
 
 pv output/images/rootfs.tar | ssh ${rootfs_server} "tar xf - -C ${rootfs_path}"
-sudo cp output/images/bzImage ${kernel_path}/bbs-${arch}-linux-${kernel_version}
-sudo chown qemu:qemu ${kernel_path}/bbs-${arch}-linux-${kernel_version}
+sudo cp output/images/bzImage ${kernel_path}/bbs-${target}-${kernel_version}
+sudo chown qemu:qemu ${kernel_path}/bbs-${target}-${kernel_version}
