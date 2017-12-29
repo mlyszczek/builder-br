ac74898e26d3c5ba16d891d157e0c7bc5daf31aa
Michał Łyszczek <michal.lyszczek@bofc.pl>
2017-12-29 22:04:10 +0100

    Added nios2 arch (but without network)

--- deploy-image.sh
+++ deploy-image.sh
@@ -5,13 +5,20 @@ kernel_path=/boot/qemu
 kernel_version=`grep BR2_LINUX_KERNEL_VERSION .config | cut -f2 -d\"`
 rootfs_path=/var/diskless/images/bbs-${target}
 rootfs_server=root@kurwik
-zimage=`ls -1 output/images | grep -i image`
+
+kernel=`ls -1 output/images | grep -i image`
+
+if [ -z ${kernel} ]
+then
+    kernel="vmlinux"
+fi
+
 dtb=`ls -1 output/images | grep .dtb`
 
 echo "This is very-local script check paths before you continue! Will execute:"
 
 echo -e "\tpv output/images/rootfs.tar | ssh ${rootfs_server} \"tar xf - -C ${rootfs_path}\""
-echo -e "\tsudo cp output/images/${zimage} ${kernel_path}/bbs-${target}-${kernel_version}"
+echo -e "\tsudo cp output/images/${kernel} ${kernel_path}/bbs-${target}-${kernel_version}"
 
 if [ ! -z "${dtb}" ]
 then
@@ -25,7 +32,7 @@ echo "Hit enter if this looks good to you. Otherwise edit this scripts settings"
 read
 
 pv output/images/rootfs.tar | ssh ${rootfs_server} "tar xf - -C ${rootfs_path}"
-sudo cp output/images/${zimage} ${kernel_path}/bbs-${target}-${kernel_version}
+sudo cp output/images/${kernel} ${kernel_path}/bbs-${target}-${kernel_version}
 sudo chown qemu:qemu ${kernel_path}/bbs-${target}-${kernel_version}
 
 if [ ! -z "${dtb}" ]
