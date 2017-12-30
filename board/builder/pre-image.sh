34dcb138baeee2804957e0fa94ebe0a4a60193ef
Michał Łyszczek <michal.lyszczek@bofc.pl>
2017-12-30 13:15:06 +0100

    Added x86_64 arch

--- board/builder/pre-image.sh
+++ board/builder/pre-image.sh
@@ -9,8 +9,11 @@ target=`echo ${hostname} | cut -f2- -d-`
 
 echo "127.0.0.1  ${hostname}.kurwinet.pl ${hostname} localhost" > ${target_dir}/etc/hosts
 echo "arch ${arch} 200" >> ${target_dir}/etc/opkg/opkg.conf
+###
+# change _ to - in for example x86_64 as opkg doesn't support _ in names
+#
+target=`echo ${target} | sed 's/_/-/'`
 echo "native-${target}" >> ${target_dir}/etc/packages
 echo "buildbot-slaves-${target}" >> ${target_dir}/etc/packages
-
 
 exit 0
