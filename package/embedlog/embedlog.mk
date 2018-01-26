deb7517013a86f170a2cf2948a77ace5d0a6a127
Michał Łyszczek <michal.lyszczek@bofc.pl>
2018-01-26 22:47:25 +0100

    add: embedlog as dependency build for projects

---
+++ package/embedlog/embedlog.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# embedlog
+#
+################################################################################
+
+EMBEDLOG_VERSION = 0.2.0
+EMBEDLOG_SOURCE = embedlog-$(EMBEDLOG_VERSION).tar.gz
+EMBEDLOG_SITE = http://distfiles.kurwinet.pl/embedlog
+EMBEDLOG_INSTALL_STAGING = YES
+EMBEDLOG_LICENSE = BSD-2-Clause
+EMBEDLOG_LICENSE_FILES = LICENSE
+
+$(eval $(autotools-package))
