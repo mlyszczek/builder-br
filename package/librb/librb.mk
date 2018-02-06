51e73f43fe3947418f8561f6dc5d7b85ad883f93
Michał Łyszczek <michal.lyszczek@bofc.pl>
2018-02-06 13:52:01 +0100

    update librb to 3.0.0

--- package/librb/librb.mk
+++ package/librb/librb.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LIBRB_VERSION = 2.1.2
+LIBRB_VERSION = 3.0.0
 LIBRB_SOURCE = librb-$(LIBRB_VERSION).tar.gz
 LIBRB_SITE = http://distfiles.kurwinet.pl/librb
 LIBRB_INSTALL_STAGING = YES
 LIBRB_LICENSE = BSD-2-Clause
-LIBRB_LICENSE_FILES = LICENSE
+LIBRB_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
