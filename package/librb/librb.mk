e1a3df4acce517fe17d6aafe06768ae857d136f9
Michał Łyszczek <michal.lyszczek@bofc.pl>
2018-02-10 08:28:59 +0100

    librb moved to semantic versioning so did we

--- package/librb/librb.mk
+++ package/librb/librb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRB_VERSION = 3.0.0
+LIBRB_VERSION = 1.0.0
 LIBRB_SOURCE = librb-$(LIBRB_VERSION).tar.gz
 LIBRB_SITE = http://distfiles.kurwinet.pl/librb
 LIBRB_INSTALL_STAGING = YES
