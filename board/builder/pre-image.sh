67d6ffff07aa7ae956ef1ac1779442f669db4d2f
Michał Łyszczek <michal.lyszczek@bofc.pl>
2018-09-21 23:26:59 +0200

    fix: compiler is not bound to hostname

--- board/builder/pre-image.sh
+++ board/builder/pre-image.sh
@@ -6,6 +6,10 @@ hostname=`grep BR2_TARGET_GENERIC_HOSTNAME=\" ${BR2_CONFIG} | cut -f2 -d\"`
 arch=`echo ${hostname} | cut -f2 -d-`
 target=`echo ${hostname} | cut -f2- -d-`
 
+compiler="$(echo ${target} | cut -d- -f1)"
+compiler+="-builder-"
+compiler+="$(echo ${target} | cut -d- -f3-)"
+
 # install includes as they are build dependencies for packages we test
 mkdir "${target_dir}/usr/include"
 cp "${STAGING_DIR}/usr/include/rb.h" "${target_dir}/usr/include"
@@ -13,11 +17,13 @@ cp "${STAGING_DIR}/usr/include/embedlog.h" "${target_dir}/usr/include"
 
 echo "127.0.0.1  ${hostname}.kurwinet.pl ${hostname} localhost" > ${target_dir}/etc/hosts
 echo "arch ${arch} 200" >> ${target_dir}/etc/opkg/opkg.conf
+
 ###
 # change _ to - in for example x86_64 as opkg doesn't support _ in names
 #
 target=`echo ${target} | sed 's/_/-/'`
-echo "native-${target}" >> ${target_dir}/etc/packages
-echo "buildbot-slaves-${target}" >> ${target_dir}/etc/packages
+compiler=`echo ${compiler} | sed 's/_/-/'`
 
+echo "buildbot-slaves-${target}" >> ${target_dir}/etc/packages
+echo "native-${compiler}" >> ${target_dir}/etc/packages
 exit 0
